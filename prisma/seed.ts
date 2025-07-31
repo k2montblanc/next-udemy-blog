a20efb3123462a997230de26e616159875741cec
k2montblanc <keisuke.nemoto.i625@dirbato.co.jp>
2025-07-31 16:32:00 +0900

    prisma setting

---
+++ prisma/seed.ts
@@ -0,0 +1,53 @@
+// primsa.対象テーブル名.メソッド のように記述 
+import { PrismaClient } from '../src/generated/prisma'
+import * as bcrypt from 'bcryptjs' 
+
+const prisma = new PrismaClient()
+
+async function main() {  
+  await prisma.post.deleteMany() //クリーンアップ
+  await prisma.user.deleteMany() //クリーンアップ
+
+  const hashedPassword = await bcrypt.hash('password123', 12) // 暗号化 
+
+  // ダミー画像
+  const dummyImages = [
+    'https://picsum.photos/seed/post1/600/400', 
+    'https://picsum.photos/seed/post2/600/400'
+  ]
+  
+  // ユーザー作成 
+  const user = await prisma.user.create({ 
+    data: { 
+      email: 'test@example.com', 
+      name: 'Test User', 
+      password: hashedPassword, 
+      posts: { 
+        create: [ 
+          { title: 'はじめてのブログ投稿', 
+            content: 'これは最初のブログ投稿です。Next.jsとPrismaでブログを作成しています。', 
+            topImage: dummyImages[0], 
+            published: true, 
+          }, 
+          { title: '2番目の投稿', 
+            content: 'ブログの機能を少しずつ追加していきます。認証機能やダッシュボードなども実装予定です。', 
+            topImage: dummyImages[1], 
+            published: true, 
+          }
+        ]
+      }
+    }
+  }) 
+
+  console.log({ user })
+}
+
+// main() の呼び出しは関数外で一度だけ行う
+  main()
+    .catch((e) => {
+      console.error(e)
+      process.exit(1)
+    }) 
+    .finally(async () => {
+      await prisma.$disconnect()
+    })
